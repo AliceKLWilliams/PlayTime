f9e691b3e883aafceb23f6d4266d57a590580ac8
Alice Williams <aliceklwilliams@gmail.com>
2018-10-22 20:35:42 +0100

    Change mobile navigation style to drop from the top of the screen, instead of using a fading modal.

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -3,21 +3,11 @@ const navClose = document.querySelector(".nav__close");
 const navList = document.querySelector(".nav__items");
 
 navButton.addEventListener("click", () => {
-	navList.classList.add("nav__items--transition-show");
-	requestAnimationFrame((ts) => {
-		navList.classList.add("nav__items--show")
-	});
+	navList.classList.add("nav__items--show");
 });
 
 navClose.addEventListener("click", () =>{
-	navList.classList.remove("nav__items--transition-show");
-	navList.classList.add("nav__items--transition-hide");
-
-	requestAnimationFrame((ts) => {
-		navList.classList.remove("nav__items--show");
-	});
-
-	setTimeout(() => navList.classList.remove("nav__items--transition-hide"), 501); //clean up classes
+	navList.classList.remove("nav__items--show");
 });
 
 window.addEventListener("resize", () => {
